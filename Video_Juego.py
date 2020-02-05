3ad4165ccd1ac76328ee013da6fbe72c0ed8bee8
pedro Art <pedregales5@gmail.com>
2020-02-04 20:44:40 -0400

    Agregamos la bolita.png y sale en la esquina superior izquierda

--- Video_Juego.py
+++ Video_Juego.py
@@ -7,15 +7,29 @@ import sys
 ancho = 640
 alto = 480
 
+class Bolita(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        # Cargar Imagen
+        self.image = pygame.image.load('imagenes/bolita.png')
+        # Obtener rectangulo de la imagen
+        self.rect = self.image.get_rect()
+
 # Iniciando pantalla
 pantalla = pygame.display.set_mode((ancho,alto))
 
 # Cambiaremos titulo de pantalla
 pygame.display.set_caption('Juego de ladrillos')
+
+bolita = Bolita()
+
 while True:
     # Revisar todos los eventos
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             sys.exit()
 
+    #Dibujar bolita en pantalla
+    pantalla.blit(bolita.image, bolita.rect)
+    #Actualizar los elementos en pantalla
     pygame.display.flip()
