862cc7a57a8c1cbdca2536ac878902cf8bfba2f6
pedro Art <pedregales5@gmail.com>
2020-02-04 21:10:11 -0400

    movemos la posicion inicial de la bolita.png al centro de la pantalla

--- Video_Juego.py
+++ Video_Juego.py
@@ -10,10 +10,16 @@ alto = 480
 class Bolita(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+
         # Cargar Imagen
         self.image = pygame.image.load('imagenes/bolita.png')
+        
         # Obtener rectangulo de la imagen
         self.rect = self.image.get_rect()
+
+        # Posicion inicial centrada en pantalla
+        self.rect.centerx = ancho/2
+        self.rect.centery = alto/2
 
 # Iniciando pantalla
 pantalla = pygame.display.set_mode((ancho,alto))
@@ -31,5 +37,6 @@ while True:
 
     #Dibujar bolita en pantalla
     pantalla.blit(bolita.image, bolita.rect)
+
     #Actualizar los elementos en pantalla
     pygame.display.flip()
